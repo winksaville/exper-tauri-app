79f579d2ee5cad07f9720f6aa2396c00bf5edba6
Wink Saville <wink@saville.com>
2024-05-24 14:56:20 -0700

    feature: Initial Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Tauri + Vanilla
+
+This template should help get you started developing with Tauri in vanilla HTML, CSS and Javascript.
+
+
+## Recommended IDE Setup
+
+- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+
+## Development
+
+At the command line cd to src-tauri/ directory and run:
+
+```bash
+cargo tauri dev
+```
+
+![Image tauri-app](assets/tauri-app.1.png)
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or http://apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
+
+
---
+++ src-tauri/src/main.rs
@@ -0,0 +1,27 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+#[tauri::command]
+fn greet(name: &str) -> String {
+    format!("Hello, {}! You've been greeted from Rust!", name)
+}
+
+fn main() {
+    // Disable the DMABuf renderer to run on my 3900x Linux box.
+    // Run one of these from within src-tauri/:
+    //     - cargo tauri dev
+    //     - cargo tauri build -b none
+    // The first is "developer mode" and the second is "production mode"
+    // in the second case you can run `./target/release/tauri-app` to start the app.
+    //
+    // If you comment out the line and do one of to get the same results as above:
+    //     - WEBKIT_DISABLE_DMABUF_RENDERER=1 cargo tauri dev
+    //     - WEBKIT_DISABLE_DMABUF_RENDERER=1 cargo tauri build -b none
+    std::env::set_var("WEBKIT_DISABLE_DMABUF_RENDERER", "1");
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
